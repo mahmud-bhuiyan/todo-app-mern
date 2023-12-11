e083c6cb9397cbfcfd59131e00d6d5ab95197c34
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-11 18:46:08 +0600

    login function updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,15 +4,21 @@ const jwt = require("jsonwebtoken");
 const asyncWrapper = require("../middlewares/asyncWrapper");
 const { createCustomError } = require("../errors/customError");
 
-// custom user details
-const customUserDetails = (user) => {
-  const { _id, name, email } = user;
-  return { _id, name, email };
+const generateAndSetToken = (res, user) => {
+  // Creating JWT payload
+  const payload = { user: user._id };
+
+  // Generating JWT with expire time
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: 360000,
+  });
+
+  // Setting the token in a cookie with HttpOnly & expire time
+  res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
 };
 
 //register
 const registerUser = asyncWrapper(async (req, res) => {
-  // Extracting data from request body
   const { name, email, password } = req.body;
 
   // Duplicate user handling
@@ -25,16 +31,8 @@ const registerUser = asyncWrapper(async (req, res) => {
   const user = new User({ name, email, password });
   await user.save();
 
-  // Creating JWT payload
-  const payload = { user: user._id };
-
-  // Generating JWT with expire time
-  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-    expiresIn: 360000,
-  });
-
-  // Setting the token in a cookie with HttpOnly & expire time
-  res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
+  // Generate and set token
+  generateAndSetToken(res, user);
 
   // displaying user details without password
   const { password: pass, ...rest } = user._doc;
@@ -55,19 +53,44 @@ const loginUser = asyncWrapper(async (req, res) => {
   const isMatch = await bcrypt.compare(password, user.password);
 
   if (!isMatch) {
-    throw createCustomError("Invalid credentials", 401);
+    throw createCustomError("Invalid credentials", 400);
   }
 
-  // user details
-  const userDetails = customUserDetails(user);
+  // Generate and set token
+  generateAndSetToken(res, user);
 
-  const token = jwt.sign(
-    {
-      _id: user._id.toString(),
-    },
-    process.env.JWT_SECRET_KEY
-  );
-  res.send({ user: userDetails, token, message: "Logged in successfully" });
+  // displaying user details without password
+  const { password: pass, ...rest } = user._doc;
+
+  // Sending the response with user details
+  res.status(200).json({ msg: "Logged in successfully", user: rest });
 });
+
+//login
+// const logoutUser = asyncWrapper(async (req, res) => {
+//   const { email, password } = req.body;
+//   const user = await User.findOne({ email });
+
+//   if (!user) {
+//     throw createCustomError("User not found!", 404);
+//   }
+
+//   const isMatch = await bcrypt.compare(password, user.password);
+
+//   if (!isMatch) {
+//     throw createCustomError("Invalid credentials", 401);
+//   }
+
+//   // user details
+//   const userDetails = customUserDetails(user);
+
+//   const token = jwt.sign(
+//     {
+//       _id: user._id.toString(),
+//     },
+//     process.env.JWT_SECRET_KEY
+//   );
+//   res.send({ user: userDetails, token, message: "Logged in successfully" });
+// });
 
 module.exports = { registerUser, loginUser };
